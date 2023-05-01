d6a1788dde3bbb98ad3295c9289794c5b89edcd4
Jay Luo <68299308+jayluo2@users.noreply.github.com>
2023-05-01 14:12:17 -0400

    Create get_atac_metadata.R

---
+++ src/scenicplus/get_atac_metadata.R
@@ -0,0 +1,9 @@
+# Get metadata of nucleus accumbens ATAC-seq data
+library(GenomicRanges)
+
+atac_rds = "/path/to/NAc.snATAC.ReMACS2.Motifs.ChromVAR.rds"
+output_dir = '/path/to/output_dir/'
+  
+atac_rds_data <- readRDS(atac_rds)
+
+write.csv(atac_rds_data@meta.data, paste0(output_dir, "data/nucleus_accumbens_metadata.csv"))
